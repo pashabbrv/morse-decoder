ba1e8a9db5669a1a0db44785ab42be8c0bc5c821
pashabbrv <112647982+pashabbrv@users.noreply.github.com>
2024-09-25 16:30:56 +0300

    fix: fix decode function

--- src/index.js
+++ src/index.js
@@ -1,77 +1,25 @@
-const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
-
 function decode(expr) {
     let result = '';
 
-    for(let i = 0; i < expr.length; i+=10){
-        let part = expr.slice(i, i+10);
+    for (let i = 0; i < expr.length; i += 10) {
+        let part = expr.slice(i, i + 10);
         
-        if(part === "**********"){
+        if (part === "**********") {
             result += ' ';
-        }
-
-        else{
+        } else {
             let symbol = '';
-            let part2 = '';
-
-            for(let k = 0; k < expr.length; i+=2){
-                part2 = part.slice(k, k+2)
+            for (let k = 0; k < part.length; k += 2) {
+                let part2 = part.slice(k, k + 2);
                 
-                if(part2 === '10'){
-                    symbol+= '.';
-                }
-
-                else if(part2 === '11'){
-                    symbol += '-'
+                if (part2 === '10') {
+                    symbol += '.';
+                } else if (part2 === '11') {
+                    symbol += '-';
                 }
             }
-
             result += MORSE_TABLE[symbol];
-
         }
-
     }
 
     return result;
-}
-
-module.exports = {
-    decode
 }
