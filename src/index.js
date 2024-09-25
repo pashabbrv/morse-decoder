2986c6bc68cb89efe4572e59bd2ae2dcb2c24502
pashabbrv <112647982+pashabbrv@users.noreply.github.com>
2024-09-25 16:11:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    for(let i = 0; i < expr.length; i+=10){
+        let part = expr.slice(i, i+10);
+        
+        if(part=="**********"){
+            result += ' ';
+        }
+
+        else{
+            let symbol = '';
+            let part2 = '';
+
+            for(let k = 0; k < expr.length; i+=2){
+                part2 = part.slice(k, k+2)
+                
+                if(part2 == '10'){
+                    symbol+= '.';
+                }
+
+                else if(part2 == '11'){
+                    symbol += '-'
+                }
+            }
+
+            result += MORSE_TABLE[symbol];
+
+        }
+
+    }
+
+    return result;
 }
 
 module.exports = {
